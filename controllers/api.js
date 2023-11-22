6356f37b76034b63c7766a3057433562b91ad96c
Akinlua <akinluaolorunfunminiyi@gmail.com>
2023-11-23 00:32:08 +0100

    new commit

--- controllers/api.js
+++ controllers/api.js
@@ -15,10 +15,60 @@ const stripePublickey = process.env.STRIPE_PUBLIC_KEY
 const stripe = require('stripe')(stripeSecretkey);
 
 const getAllItemDetails = async (req, res) => {
-    const items = await Item.find({})   
+    try {
+        const items = await Item.find({})   
 
-    const list = []
-    for (const item of items){
+        const list = []
+        for (const item of items){
+    
+            // save image
+            const imageUrl = item.pic_path;
+            const folderToSave = './public/item-images'; // Specify the folder path where you want to save the image
+            const imageNameToSave = item.owner + "-" + item.id + '.png';
+    
+            await saveImageFromUrl(imageUrl, folderToSave, imageNameToSave);
+    
+            
+            const validIds = {}
+            for (verified in item.verified_Bidders) {
+                validIds[ item.verified_Bidders[verified].biderId] =  item.verified_Bidders[verified].bider_email 
+            }
+    
+            const res = {
+                id: item.id,
+                product: item.name,
+                description: item.description,
+                startPrice: item.cost,
+                image: `${url}/item-images/${imageNameToSave}`,
+                validIds: validIds
+            }
+    
+            list.push(res)
+        }
+    
+        res.status(200).json(list)
+    } catch(error){
+        res.status(500).json({
+            message: 'An error occured',
+            error: error
+        })
+    }
+   
+}
+
+const getItemDetails = async (req, res) => {
+
+    try {
+        const {id} = req.params
+
+        const item = await Item.findOne({id: id})
+
+        if(!item){
+            return res.status(404).json({
+                status: 404,
+                message: "Item not found"
+            })
+        }
 
         // save image
         const imageUrl = item.pic_path;
@@ -27,110 +77,84 @@ const getAllItemDetails = async (req, res) => {
 
         await saveImageFromUrl(imageUrl, folderToSave, imageNameToSave);
 
-        
         const validIds = {}
         for (verified in item.verified_Bidders) {
             validIds[ item.verified_Bidders[verified].biderId] =  item.verified_Bidders[verified].bider_email 
         }
-
-        const res = {
+        // console.log(validIds)
+        res.status(200).json({
             id: item.id,
             product: item.name,
             description: item.description,
             startPrice: item.cost,
             image: `${url}/item-images/${imageNameToSave}`,
             validIds: validIds
-        }
-
-        list.push(res)
-    }
-
-    res.status(200).json(list)
-}
-
-const getItemDetails = async (req, res) => {
-
-    const {id} = req.params
-
-    const item = await Item.findOne({id: id})
-
-    if(!item){
-        return res.status(404).json({
-            status: 404,
-            message: "Item not found"
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: 'An error occured',
+            error: error
         })
     }
-
-    // save image
-    const imageUrl = item.pic_path;
-    const folderToSave = './public/item-images'; // Specify the folder path where you want to save the image
-    const imageNameToSave = item.owner + "-" + item.id + '.png';
-
-    await saveImageFromUrl(imageUrl, folderToSave, imageNameToSave);
-
-    const validIds = {}
-    for (verified in item.verified_Bidders) {
-        validIds[ item.verified_Bidders[verified].biderId] =  item.verified_Bidders[verified].bider_email 
-    }
-    // console.log(validIds)
-    res.status(200).json({
-        id: item.id,
-        product: item.name,
-        description: item.description,
-        startPrice: item.cost,
-        image: `${url}/item-images/${imageNameToSave}`,
-        validIds: validIds
-    })
+    
 }
 
 const setWinner = async (req, res) => {
 
-    const {id} = req.params;
-    const {password, winnerId, winner_email}  = req.body
+    try {
+        const {id} = req.params;
+        const {password, winnerId, winner_email}  = req.body
 
-    const item = await Item.findOne({id: id})
-    if(!item){
-        return res.status(404).json({
-            status: 404,
-            message: "Item not found"
-        })
-    }
-
-    if(!password || password != "GMGcheatpost38895"){
-        return res.status(401).json({
-            status: 401,
-            message: "Unauthorized to perform this action"
-        })
-    }
-
-    // make sure winner is part of the verified user
-    var verify = 0
-    var winner, winner_hold_id
-    item.verified_Bidders.forEach(verified => {
-        if(verified.biderId == winnerId && verified.bider_email == winner_email){
-            winner = verified.bider
-            winner_hold_id = verified.bider_holdId
-            return verify = 1
+        const item = await Item.findOne({id: id})
+        if(!item){
+            return res.status(404).json({
+                status: 404,
+                message: "Item not found"
+            })
         }
-     });
-    if(verify < 1){
-        return res.status(404).json({
-            status: 404,
-            message: "The body provided doesn't match with details in our database"
-        }) 
-    }
-    item.winner = winner
-    item.winnerId = winnerId
-    item.winnerEmail = winner_email
-    item.winnerHoldId = winner_hold_id
-    await item.save()
 
-    res.status(200).json({
-        status: 200,
-        message: `Winner (${winner}) succesfully declared for the auction of ${item.name}`,
-        winner: winner,
-        winner_email: winner_email
-    })
+        if(!password || password != "GMGcheatpost38895"){
+            return res.status(401).json({
+                status: 401,
+                message: "Unauthorized to perform this action"
+            })
+        }
+
+        // make sure winner is part of the verified user
+        var verify = 0
+        var winner, winner_hold_id
+        item.verified_Bidders.forEach(verified => {
+            if(verified.biderId == winnerId && verified.bider_email == winner_email){
+                winner = verified.bider
+                winner_hold_id = verified.bider_holdId
+                return verify = 1
+            }
+        });
+        if(verify < 1){
+            return res.status(404).json({
+                status: 404,
+                message: "The body provided doesn't match with details in our database"
+            }) 
+        }
+        item.winner = winner
+        item.winnerId = winnerId
+        item.winnerEmail = winner_email
+        item.winnerHoldId = winner_hold_id
+        await item.save()
+
+        res.status(200).json({
+            status: 200,
+            message: `Winner (${winner}) succesfully declared for the auction of ${item.name}`,
+            winner: winner,
+            winner_email: winner_email
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: 'An error occured',
+            error: error
+        })
+    }
+
 }
 
 module.exports = {
